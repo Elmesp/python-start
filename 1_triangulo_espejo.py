5c7a1d933e2143d77cb074505c41cacc70b64303
elmesp <elmesp@gmail.com>
2018-05-31 03:20:46 +0000

    Resuelto ejercicio 1

--- 1_triangulo_espejo.py
+++ 1_triangulo_espejo.py
@@ -1,18 +1,40 @@
-# print('Ingrese tamaño: ')
-tamanoInput = input()
-tamano = int(tamanoInput)
-
-# print('Ingrese caracter: ')
+n = int(input())
 caracter = input()
+contador = 0
 
-for i in range(1, tamano + 1):
-    espacios = tamano - i
-    linea = ''
+for i in range(n):
+    blanco = n - i - 1
+   #print(blanco)
+   
+    for j in range(blanco):
+        print(' ',end='')
+       
+    repeticion = n - blanco
     
-    for j in range(0, espacios):
-        linea += ' '
+    for j in range(repeticion):
+        print(caracter,end='')
+        contador = contador + 1
     
-    for j in range(0, i):
-        linea += caracter
-        
-    print(linea)
+    print()
+
+print('La cantidad de', caracter, 'es', contador)
+   
+    # la cantidad de + es 15
+   
+   
+   
+    
+    #for j in range(n - i - 1):
+    #    print('',end='')
+    
+#    print('')
+#print (caracter)
+
+
+# n = 5             blanco = n - i - 1     repeticion = n - blanco, i +1
+#  iteracion       n espacios blanco      repeticion por varias veces
+#  i = 0                 4                         1
+#  i = 1                 3                         2
+#  i = 2                 2                         3
+#  i = 3                 1                         4
+#  i = 4                 0                         5
